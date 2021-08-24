30ed82f54c6b35d2cfcba895efe0d8050ba840db
yosrigam <s33636@beuth-hochschule.de>
2021-08-24 20:32:41 +0200

    add Sign Up button to login template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,6 +39,10 @@
                     </a>
                 @endif
 
+                    <a type="button" href="{{ route('register') }}" class="ml-4 inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-300 disabled:opacity-25 transition">
+                        SIGNUP
+                    </a>
+
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>

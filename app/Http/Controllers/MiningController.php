c13dbf265c3084900850a0b38d2819db11749d96
yosrigam <s33636@beuth-hochschule.de>
2021-08-25 20:36:44 +0200

    create mining controller

---
+++ app/Http/Controllers/MiningController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UnspalshPhotos;
+use App\Models\UnspalshUsers;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Unsplash;
+
+class MiningController extends Controller
+{
+    public function search(Request $request){
+
+        \Unsplash\HttpClient::init([
+            'applicationId'	=> env("UNSPLASH_API_KEY"),
+            'secret'		=> env("UNSPLASH_SECRET_KEY"),
+            'callbackUrl'	=> 'http://unsplash-api.com/',
+            'utmSource' => 'SPACESQUAD'
+        ]);
+
+
+        $search = $request['search-term'];
+        $page = 2;
+        $per_page = 1000;
+
+        $photos = Unsplash\Search::photos($search, $page, $per_page);
+        $users = Unsplash\Search::users($search, $page, $per_page);
+            $this->saveUsers($users->getResults());
+            $this->savePhotos($photos->getResults());
+            return view('spacesquad.search')->with('users', $users)->with('photos',$photos);
+
+    }
+
+
+
+    private function saveUsers($users){
+        foreach($users as $user){
+            if(!UnspalshUsers::find($user['id'])){
+            UnspalshUsers::create($user);
+                var_dump('user saved');
+            }
+        }
+    }
+
+    private function savePhotos($photos){
+        foreach($photos as $photo){
+            //dd($photo);
+            if (!UnspalshPhotos::find($photo['id'])){
+                UnspalshPhotos::create($photo);
+                var_dump('photo saved');
+            }
+        }
+    }
+}
